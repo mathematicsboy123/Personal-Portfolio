286b61ace4dc31bdbf43a67b98a20b00071c08b0
mathematicsboy123 <shreyansdaga007@gmail.com>
2023-04-05 16:30:58 -0400

    [UPDATE] First commit message from ubuntu command lie

---
+++ src/Components/Nav.js
@@ -0,0 +1,13 @@
+import "./Nav.css";
+
+function Nav() {
+    return (
+        <div>
+            <ul>
+                <li>nav</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
